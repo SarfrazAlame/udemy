771b39c24f09757a015b755722c2e311c4d87d1e
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-06-07 09:18:40 +0530

    ui

---
+++ src/app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex h-full items-center">
+      <Image src="/logo.svg" height={100} width={100} alt="logo" />
+      <span className="text-3xl">
+        <span className="text-pink-800">U</span>demy
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/app/(dashboard)/_components/SidebarRoutes.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { Layout } from 'lucide-react'
+import React from 'react'
+
+const guestRoutes = [
+    {
+        icon:Layout,
+    }
+]
+
+const SidebarRoutes = () => {
+  return (
+    <div>SidebarRoutes</div>
+  )
+}
+
+export default SidebarRoutes
