18cfbe654dd331dfa86372137b757c4b968b7e70
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-06-07 12:11:53 +0530

    creating course

--- src/app/(dashboard)/_components/Logo.tsx
+++ src/app/(dashboard)/_components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="flex h-full items-center">
       <Image src="/logo.svg" height={100} width={100} alt="logo" />
-      <span className="text-3xl">
+      <span className="text-3xl text-sky-700">
         <span className="text-pink-800">U</span>demy
       </span>
     </div>
