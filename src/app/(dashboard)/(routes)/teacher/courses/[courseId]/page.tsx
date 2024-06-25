1c6be8568032bcc69ef71e86419b1d9a1382b858
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-06-25 09:04:52 +0530

    price

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,11 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+  CircleDollarSign,
+  File,
+  LayoutDashboard,
+  ListChecks,
+} from "lucide-react";
 import { redirect } from "next/navigation";
 import React from "react";
 import TitleForm from "./_components/TitleForm";
@@ -92,6 +97,11 @@ const page = async ({ params }: { params: { courseId: string } }) => {
             </div>
             <PriceForm initialData={course} courseId={course.id} />
           </div>
+          <div className="flex items-center gap-x-2 ">
+            <IconBadge icon={File} />
+            <h2 className="text-xl">Resources & Attachments</h2>
+          </div>
+          <ImageForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
